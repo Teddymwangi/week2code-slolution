f04a6bb8450f0c80b5df6e0217dda9fd6c8a97bf
TED <teddy.maina@student.moringaschool.com>
2023-10-31 12:29:23 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const animalList = document.getElementById("animal-list");
+    const animalDetails = document.getElementById("animal-details");
+
+    // Fetch animal data from the server
+    fetch("http://localhost:3000/characters")
+        .then(response => response.json())
+        .then(animals => {
+            // Display animal names in the list
+            animals.forEach(animal => {
+                const listItem = document.createElement("li");
+                listItem.textContent = animal.name;
+                listItem.addEventListener("click", () => {
+                    // Display animal details when clicked
+                    showAnimalDetails(animal);
+                });
+                animalList.appendChild(listItem);
+            });
+        })
+        .catch(error => console.error("Error fetching animal data:", error));
+
+    // Function to display animal details
+    function showAnimalDetails(animal) {
+        animalDetails.innerHTML = `
+            <h2>${animal.name}</h2>
+            <img src="${animal.image}" alt="${animal.name}">
+            <p>Votes: <span id="votes">${animal.votes}</span></p>
+            <button id="vote-btn">Vote</button>
+        `;
+
+        // Add event listener to vote button
+        const voteBtn = document.getElementById("vote-btn");
+        voteBtn.addEventListener("click", () => {
+            animal.votes++;
+            document.getElementById("votes").textContent = animal.votes;
+        });
+    }
+});
